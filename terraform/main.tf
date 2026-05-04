bcae74069b0c5a6cbe5cb51c1e2b8981d1655ef4
Poornima-Talawar <poornimatalawar1999@gmail.com>
2026-05-04 13:20:16 +0530

    Provisioned infra in main.tf file

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_container_registry" "acr" {
+ name = var.acr_name
+ resource_grup_name = var.resource_group_name
+ location = var.location
+ sku = "Premium"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "flaskacr1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+resource "azurerm_role_assignment" "rbac" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
